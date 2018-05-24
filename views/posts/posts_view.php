4f3e34903529258332ca2bd6c9c5998112e2f51f
ulleputtsepp <ulle.puttsepp@khk.ee>
2018-05-24 03:00:06 +0300

    Add comment function

--- views/posts/posts_view.php
+++ views/posts/posts_view.php
@@ -3,14 +3,39 @@
         <div class="media">
             <div class="media-body">
             <h4 class= "media-heading"><?php echo $post['post_subject'];?></h4>
-            <p class=""text-right"><?php echo $post['name'];?></p>
+            <p class="text-right"><?php echo $post['name'];?></p>
             <p><?php echo $post['post_text'];?></p>
             <ul class="list-inline list-unstyled">
-                <li><span><i class="glyphicon glyphicon-calendar"><?php echo $post['post_created'];?></i></span></li>
+                <li><span><i class="glyphicon glyphicon-calendar"></i><?php echo $post['post_created'];?></span></li>
                 <li></li>
-                <span><i class="glyphicon glyphicon-comment"></i> ? comments</span>
+                <?php foreach($tags as $tag):?>
+                <a href="#">
+                    <span class="label label-info">
+                        <?php echo $tag['tag_name']; ?>
+                    </span>
+                </a>&nbsp;
+                <?php endforeach?>
+                <li>|</li>
+                <span><i class="glyphicon glyphicon-comment"></i><?php echo $comment['comment_text'];?></span>
             </ul>
+            </div>
         </div>
     </div>
 </div>
 
+  <div class="container">
+      <div class="row">
+          <div class="media comment-box">
+              <div class="media-body">
+
+                  <?php foreach($comments as $comment): ?>
+                  <h4 class="media-heading"><?php echo $comment['comment_subject']?></h4>
+                  <p><?php echo $comment['comment_text']?></p>
+                  <span class="badge badge-success">Commented on <?php echo $comment['comment_created']?></span>
+                  <span class="badge badge-success">Author: <?php echo $comment['comment_author']?></span>
+                  <hr>
+                  <?php endforeach?>
+              </div>
+          </div>
+      </div>
+  </div>
